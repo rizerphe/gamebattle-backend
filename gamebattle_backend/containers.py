9e82ba82889ef185aa7a1be101691c138d593c76
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-10-14 16:57:06 +0300

    Increase mem limit

--- gamebattle_backend/containers.py
+++ gamebattle_backend/containers.py
@@ -22,7 +22,7 @@ class Limits:
     @classmethod
     def default(cls) -> Limits:
         """The default resource limits."""
-        return cls(cpu_fraction=0.1, memory_mb=80)
+        return cls(cpu_fraction=0.1, memory_mb=256)
 
 
 @dataclass
